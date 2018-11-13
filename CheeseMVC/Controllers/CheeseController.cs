0e932890db1b489e298f155caf48ebf226139af1
Jonah Martinez <Jonah.Martinez01@outlook.com>
2018-11-12 20:39:18 -0500

    Implemented ability to add users own cheeses

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+        static private List<string> Cheeses = new List<string>();
+
+        public IActionResult Index()
+        { 
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name)
+        {
+            //add the new cheese to my existing cheeses
+            Cheeses.Add(name);
+
+            return Redirect("/Cheese");
+        }
+    }
+}
